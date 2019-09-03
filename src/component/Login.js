f95b1c0d1a8cf58c7bcb7e954a4410d0cd5b8947
MarinesLemus007 <marineslemus@gmail.com>
2019-09-03 09:47:57 -0400

    Se creó vista de registro

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../img/logo3.png';
+import { Link } from 'react-router-dom';
 import '../App.css';
 
 function Login(props){
@@ -16,10 +17,10 @@ function Login(props){
                     <form className="login-form">
                         <input type="email" className="login-email" placeholder="&#9993; correo@example.com" name="email"/>
                         <input type="password" className="login-password" placeholder="&#128272; Contraseña" name="password"/>
-                        <a href="#/resetPassword" id="changePassword" className="login-text">¿Olvidaste la contraseña?</a>
+                        <a href="#/resetPassword" id="changePassword" className="login-text">¿Olvidaste tu contraseña?</a>
                         <button type="button" className="login-button" id="btn-login" value="Iniciar sesión">Iniciar sesión</button>
                         <button type="button" className="login-button-two" id="buttonGoogle" value="Iniciar sesión con Google">Iniciar sesión con Google</button>
-                        <a href="#/auth" id="btn-checkin1" className="login-text-two">¿No tienes cuenta? Registrate aqui.</a>
+                        <Link to="/register" className="link"><p className="login-text-two">¿No tienes cuenta? Registrate aqui.</p></Link>
                     </form>
                 </div>
 
--- src/component/Register.js
+++ src/component/Register.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import logo from '../img/logo3.png';
+import { Link } from 'react-router-dom';
 
 function Register(props){
     return(
-        <>
-        Register
-        </>
+        <section className="register-seccion">
+            <div className="register-content">    
+                
+                <div className="register-logo">
+                    <img className="register-img" src={logo} alt="Logo Pet Lovers"/>
+                </div>
+                
+                <div id="containerForm" className="register-body">
+                    
+                    <form className="register-form">
+                        <h2>Formulario de registro</h2>
+                        <input type="text" className="register-nickname" id="nickname" placeholder="Nombre de usuario" name="nickname"/>
+                        <input type="email" className="register-email" id="email" placeholder="&#9993; correo@example.com" name="email"/>
+                        <input type="password" className="register-password" id="password" placeholder="&#128272; Contraseña" name="password"/>
+                        <input type="password" className="register-password-two" id="password_repeat" placeholder="&#128272; Repetir contraseña" name="password"/>
+                        <button type="button" className="register-button" id="btn-checkin2">Registrarse</button>
+                        <Link to="/" className="link"> <button type="button" className="register-button-two" id="back">Volver</button></Link>
+                    </form>
+
+                </div>
+
+            </div>
+        </section>
     );
 }
 
