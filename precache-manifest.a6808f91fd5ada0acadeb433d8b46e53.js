5ee89860803914d1a124ecd265b00e8606a6fe4d
MarinesLemus007 <marineslemus@gmail.com>
2019-09-03 08:32:54 -0400

    Updates

---
+++ precache-manifest.a6808f91fd5ada0acadeb433d8b46e53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8792834eb3c4e54c3f7e582ee7645b",
+    "url": "/SCL009-red-social-frameworks/index.html"
+  },
+  {
+    "revision": "25292859a045f64dced8",
+    "url": "/SCL009-red-social-frameworks/static/css/main.5f667410.chunk.css"
+  },
+  {
+    "revision": "e8f95c0d208ff9c413f3",
+    "url": "/SCL009-red-social-frameworks/static/js/2.9d222da0.chunk.js"
+  },
+  {
+    "revision": "25292859a045f64dced8",
+    "url": "/SCL009-red-social-frameworks/static/js/main.48055b4f.chunk.js"
+  },
+  {
+    "revision": "a9b42562a2f20ac3095f",
+    "url": "/SCL009-red-social-frameworks/static/js/runtime~main.2acb455e.js"
+  },
+  {
+    "revision": "b970952117aafd934ed02f4926be0610",
+    "url": "/SCL009-red-social-frameworks/static/media/logo3.b9709521.png"
+  }
+]);
