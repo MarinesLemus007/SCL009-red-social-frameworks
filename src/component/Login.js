b15bc9529c2b6fb2e8655f75eb7f53a61fb813b4
MarinesLemus007 <marineslemus@gmail.com>
2019-09-04 16:43:08 -0400

    Login con gmail usando librería

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,32 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from '../img/logo3.png';
 import { Link } from 'react-router-dom';
 
-function Login(props){
-    return(
-        
-         <section className="login-seccion">
-            <div className="login-content">
-                
-                <div className="login-logo">
-                    <img className="login-img" src={logo} alt="Logo Pet Lovers"/>
-                </div>
-            
-                <div className="login-body">
-                    <form className="login-form">
-                        <input type="email" className="login-email" placeholder="&#9993; correo@example.com" name="email"/>
-                        <input type="password" className="login-password" placeholder="&#128272; Contraseña" name="password"/>
-                        <Link to="/forgetpassword" className="link"><p className="login-text">¿Olvidaste tu contraseña?</p></Link>
-                        <Link to="/home" className="link"><button type="button" className="login-button" id="btn-login" value="Iniciar sesión">Iniciar sesión</button></Link>
-                        <button type="button" className="login-button-two" id="buttonGoogle" value="Iniciar sesión con Google">Iniciar sesión con Google</button>
-                        <Link to="/register" className="link"><p className="login-text-two">¿No tienes cuenta? Registrate aqui.</p></Link>
-                    </form>
-                </div>
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from '../firebase';
 
-            </div>
-        </section>
-        
-    );
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+class Login extends Component{
+    render(){
+        const { user, signOut, signInWithGoogle} = this.props;
+        return ( 
+            <section className="login-seccion">
+                <div className="login-content">
+                
+                    <div className="login-logo">
+                        <img className="login-img" src={logo} alt="Logo Pet Lovers"/>
+                    </div>
+            
+                    <div className="login-body">
+                        <form className="login-form">
+                            <input type="email" className="login-email" placeholder="&#9993; correo@example.com" name="email"/>
+                            <input type="password" className="login-password" placeholder="&#128272; Contraseña" name="password"/>
+                            <Link to="/forgetpassword" className="link"><p className="login-text">¿Olvidaste tu contraseña?</p></Link>
+                            <Link to="/home" className="link"><button type="button" className="login-button" id="btn-login" value="Iniciar sesión">Iniciar sesión</button></Link>
+                            <button className="login-button-two" onClick={signInWithGoogle}>Iniciar sesión con Google</button>
+                            <Link to="/register" className="link"><p className="login-text-two">¿No tienes cuenta? Registrate aqui.</p></Link>
+                        </form>
+                    </div>
+                    { user ? <p>Hello, {user.displayName}</p>: <p>Please sign in.</p>}
+                </div>
+            </section>
+        );
+    }
 }
 
-export default Login;
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+export default withFirebaseAuth({
+    providers,
+    firebaseAppAuth,
+  })(Login);
+// export default Login;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyBqhKBBY-HUDbCI6059YsmazajHE5j-Q4g",
+    authDomain: "red-social-framework-616ea.firebaseapp.com",
+    databaseURL: "https://red-social-framework-616ea.firebaseio.com",
+    projectId: "red-social-framework-616ea",
+    storageBucket: "red-social-framework-616ea.appspot.com",
+    messagingSenderId: "129135873325",
+    appId: "1:129135873325:web:3a40d88443de1855"
+}
+
+export default config;
